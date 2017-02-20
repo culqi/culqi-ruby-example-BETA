ffda2df23a6b0dabba9f467640331921195681c2
Willy Aguirre <marti1125@gmail.com>
2017-02-20 17:59:56 -0500

    update example

--- culqi_rails/app/controllers/charge_controller.rb
+++ culqi_rails/app/controllers/charge_controller.rb
@@ -1,26 +1,30 @@
 require 'culqi-ruby'
 
 class ChargeController < ApplicationController
+
+  protect_from_forgery with: :null_session
+
   def create
     Culqi.api_key = 'sk_test_UTCQSGcXW8bCyU59'
     charge = Culqi::Charge.create(
       :amount => 1000,
       :antifraud_details => {
-        :address => "Avenida Lima 1232",
-        :address_city => "LIMA",
-        :country_code => "PE",
-        :email => "will@culqi.com",
-        :first_name => "Will",
-        :last_name => "Aguirre",
+        :address => 'Avenida Lima 1232',
+        :address_city => 'LIMA',
+        :country_code => 'PE',
+        :email => 'will@culqi.com',
+        :first_name => 'Will',
+        :last_name => 'Aguirre',
         :phone_number => 3333339,
-      }
+      },
+      :email => 'will@culqi.com',
       :capture => true,
-      :currency_code => "PEN",
-      :description => "Venta de prueba",
-      :installments => 0,
-      :metadata => "",
+      :currency_code => 'PEN',
+      :description => 'Venta de prueba',
+      :installments => params[:installments],
       :source_id => params[:token_id]
     )
     render :json => charge
   end
+
 end
